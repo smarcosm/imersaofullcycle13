f571fb9b4af953f318869a24ab186895b1cedc84
smarcosm <sebastiaomarcos49@gmail.com>
2023-06-21 19:54:28 -0300

    feat: :sparkles: Criando entidade transaction

---
+++ internal/market/entity/transaction.go
@@ -0,0 +1,30 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Transaction struct {
+	ID           string
+	SellingOrder *Order
+	BuyingOrder  *Order
+	Shares       int
+	Price        float64
+	Total        float64
+	DateTime     time.Time
+}
+
+func NewTransaction(sellingOrder *Order, buyingOrder *Order, shares int, price float64) *Transaction {
+	total := float64(shares) * price
+	return &Transaction{
+		ID:           uuid.New().String(),
+		SellingOrder: sellingOrder,
+		BuyingOrder:  buyingOrder,
+		Shares:       shares,
+		Price:        price,
+		Total:        total,
+		DateTime:     time.Now(),
+	}
+}
