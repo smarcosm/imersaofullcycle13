c2edefc6d55ed1d181870f143876bc42221392bd
smarcosm <sebastiaomarcos49@gmail.com>
2023-06-24 15:36:09 -0300

    feat: :sparkles: Criado as rotas REST, os Ativos e as Carteiras

---
+++ nestjs/src/wallets/wallet-assets/wallet-assets.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { WalletAssetsService } from './wallet-assets.service';
+
+// /wallets/
+
+@Controller('wallets/:wallet_id/assets')
+export class WalletAssetsController {
+  constructor(private walletAssetsService: WalletAssetsService) {}
+
+  @Get()
+  all(@Param('wallet_id') wallet_id: string) {
+    return this.walletAssetsService.all({ wallet_id });
+  }
+
+  @Post()
+  create(
+    @Param('wallet_id') wallet_id: string,
+    @Body() body: { asset_id: string; shares: number },
+  ) {
+    return this.walletAssetsService.create({
+      wallet_id,
+      ...body,
+    });
+  }
+
+  // @Sse('events')
+  // events(@Param('wallet_id') wallet_id: string): Observable<MessageEvent> {
+  //   return this.walletAssetsService.subscribeEvents(wallet_id).pipe(
+  //     map((event) => ({
+  //       type: event.event,
+  //       data: event.data,
+  //     })),
+  //   );
+  // }
+}
