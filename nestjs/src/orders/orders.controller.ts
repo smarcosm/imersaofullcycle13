5cb2e93897349ff311f2c54e4dae16fd948af95e
smarcosm <sebastiaomarcos49@gmail.com>
2023-06-24 19:25:20 -0300

    feat: :sparkles: Criado a integracao nestjs com kafka

--- nestjs/src/orders/orders.controller.ts
+++ nestjs/src/orders/orders.controller.ts
@@ -1,12 +1,88 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  MessageEvent,
+  Param,
+  Post,
+  Sse,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
-import { InitTransactionDto } from './order.dto';
+import { InitTransactionDto, InputExecuteTransactionDto } from './order.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Observable, map } from 'rxjs';
 
-@Controller('orders')
+type ExecuteTransactionMessage = {
+  order_id: string;
+  investor_id: string;
+  asset_id: string;
+  order_type: string;
+  status: 'OPEN' | 'CLOSED';
+  partial: number;
+  shares: number;
+  transactions: {
+    transaction_id: string;
+    buyer_id: string;
+    seller_id: string;
+    asset_id: string;
+    shares: number;
+    price: number;
+  }[];
+};
+
+@Controller('wallets/:wallet_id/orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
-  @Post()
-  initTransactionDto(@Body() body: InitTransactionDto) {
-    return this.ordersService.initTrasaction(body);
+
+  @Get()
+  all(@Param('wallet_id') wallet_id: string) {
+    return this.ordersService.all({ wallet_id });
   }
+
+  @Post()
+  initTransactionDto(
+    @Param('wallet_id') wallet_id: string,
+    @Body() body: Omit<InitTransactionDto, 'wallet_id'>,
+  ) {
+    return this.ordersService.initTransaction({
+      ...body,
+      wallet_id,
+    });
+  }
+
+  @Post('execute')
+  executeTransactionRest(
+    @Param('wallet_id') wallet_id: string,
+    @Body() body: InputExecuteTransactionDto,
+  ) {
+    return this.ordersService.executeTransaction(body);
+  }
+
+  @MessagePattern('output')
+  async executeTransactionConsumer(
+    @Payload() message: ExecuteTransactionMessage,
+  ) {
+    const transaction = message.transactions[message.transactions.length - 1];
+    await this.ordersService.executeTransaction({
+      order_id: message.order_id,
+      status: message.status,
+      related_investor_id:
+        message.order_type === 'BUY'
+          ? transaction.seller_id
+          : transaction.buyer_id,
+      broker_transaction_id: transaction.transaction_id,
+      negotiated_shares: transaction.shares,
+      price: transaction.price,
+    });
+  }
+
+  // @Sse('events')
+  // events(@Param('wallet_id') wallet_id: string): Observable<MessageEvent> {
+  //   return this.ordersService.subscribeEvents(wallet_id).pipe(
+  //     map((event) => ({
+  //       type: event.event,
+  //       data: event.data,
+  //     })),
+  //   );
+  // }
 }
--- nestjs/src/orders/orders.module.ts
+++ nestjs/src/orders/orders.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDERS_PUBLISHER',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'orders',
+            brokers: ['host.docker.internal:9094'],
+            //brokers: ['kafka:29092'], // para interna do docker
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
