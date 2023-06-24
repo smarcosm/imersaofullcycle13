b509079e277af2f81a7782e5f18cf2ca5153ab4d
smarcosm <sebastiaomarcos49@gmail.com>
2023-06-24 00:24:50 -0300

    feat: :sparkles: Finalizando aplicacao go

--- go/internal/market/transformer/transformer.go
+++ go/internal/market/transformer/transformer.go
@@ -1,14 +1,14 @@
 package transformer
 
 import (
-	"github.com/smarcosm/imersao13/go/internal/market/dto"
-	"github.com/smarcosm/imersao13/go/internal/market/entity"
+	"github.com/smarcosm/imersaofullcycle/go/internal/market/dto"
+	"github.com/smarcosm/imersaofullcycle/go/internal/market/entity"
 )
 
 func TransformInput(input dto.TradeInput) *entity.Order {
 	asset := entity.NewAsset(input.AssetID, input.AssetID, 1000)
 	investor := entity.NewInvestor(input.InvestorID)
-	order := entity.NewOrder(input.OrderID, investor, asset, input.Shares, input.Price, input.OrderType)
+	order := entity.NewOrder(input.OrderID, investor, asset, input.Shares, input.CurrentShares, input.Price, input.OrderType)
 	if input.CurrentShares > 0 {
 		assetPosition := entity.NewInvestorAssetPosition(input.AssetID, input.CurrentShares)
 		investor.AddAssetPosition(assetPosition)
